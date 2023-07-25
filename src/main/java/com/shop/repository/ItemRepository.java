c9d7525d6a7ed9b855118ea2ab697664cfbc40ea
synodical <lena0323park@naver.com>
2023-07-25 11:08:23 +0900

    Refactor : querydsl

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,11 +3,14 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.support.Querydsl;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>,
+        QuerydslPredicateExecutor<Item> {
     // jpa repo는 2개의 제네릭 타입 사용
     // 첫번째 - 엔티티 타입 클래스
     // 두번째 - 기본키 타입
@@ -23,7 +26,10 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query("select i from Item i where i.itemDetail like " +
             "%:itemDetail% order by i.price desc ")
     List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
     @Query(value = "select * from item i where i.item_detail like " +
             "%:itemDetail% order by i.price desc ", nativeQuery = true)
     List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
+
 }
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,9 +1,19 @@
 package com.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.thymeleaf.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -16,6 +26,9 @@ import java.util.List;
 class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
+    @PersistenceContext
+    EntityManager em;
+    // 영속성 컨텍스트를 사용하기 위해 어노테이션을 사용해 엔티티 매니저 빈을 주입
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -113,6 +126,81 @@ class ItemRepositoryTest {
                 .findByItemDetailByNative("테스트 상품 상세 설명");
         for (Item item : itemList) {
             System.out.println(item.toString());
+        }
+    }
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void querydslTest() {
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        // querydsl을 통해 쿼리를 생성하기 위해 플러그인을 통해 자동으로 생성된 qitem 객체 사용
+
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+        List<Item> itemList = query.fetch();
+        // 쿼리 결과를 리스트로 반환
+        // fetch 실행 시점으로 쿼리문 실행
+        // 그 외: fetchOne, fetchFirst 등
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+    public void createItemList2() {
+        for (int i = 1; i < 6; i++) {
+            Item item = Item.builder()
+                    .itemNm("테스트 상품" + i)
+                    .price(10000 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .stockNumber(100)
+                    .regTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build();
+            itemRepository.save(item);
+        }
+        for (int i = 6; i <= 10; i++) {
+            Item item = Item.builder()
+                    .itemNm("테스트 상품" + i)
+                    .price(10000 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SOLD_OUT)
+                    .stockNumber(0)
+                    .regTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build();
+            itemRepository.save(item);
+        }
+    }
+    @Test
+    @DisplayName("상품 Querydsl 조회 테스트 2")
+    public void querydslTest2() {
+        this.createItemList2();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        // query에 들어갈 조건을 만들어주는 빌더
+        QItem item = QItem.item;
+        String itemDetail = "테스트 상품 상세 설명";
+        int price = 10003;
+        String itemSellStat = "SELL";
+
+        booleanBuilder.and(item.itemDetail.like("%" + itemDetail + "%"));
+        booleanBuilder.and(item.price.gt(price));
+
+        if (StringUtils.equals(itemSellStat, ItemSellStatus.SELL)) {
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+        Pageable pageable = PageRequest.of(0, 5);
+        // 데이터를 페이징해 조회
+
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println("total " + itemPagingResult.getTotalElements());
+
+        List<Item> res = itemPagingResult.getContent();
+        for (Item resItem : res) {
+            System.out.println(resItem.toString());
         }
     }
 }
